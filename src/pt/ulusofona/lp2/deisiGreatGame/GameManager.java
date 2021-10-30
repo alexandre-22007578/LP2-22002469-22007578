f921f854d51bd5b61fed3053b2a923e2990c3a6f
Daniel Granja <thedanielgranja@gmail.com>
2021-10-30 20:35:45 +0100

    imagem meta

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -71,7 +71,7 @@ public class GameManager {
 
     public String getImagePng(int position) {
         if (position==tamanhoTabueiro){
-            return "glory.png";
+            return "podium.png.jpg";
         }
         return "";
 
