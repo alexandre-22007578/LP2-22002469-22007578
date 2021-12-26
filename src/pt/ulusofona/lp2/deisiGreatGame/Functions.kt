f3eb4ce9bd0d62dc108bf0415675f8301bb880d6
Alexandre Costa <a22007578@alunos.ulht.pt>
2021-12-26 16:43:17 +0000

    primeiros passos parte 3 do projeto

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -4,29 +4,7 @@ enum class CommandType { GET, POST }
 
 fun router() {
 
-    return getFunction()
+
 
 }
 
-
-fun getPlayer(manager: GameManager, args: List<String>): String? {
-
-    when (args[0]) {
-        "PLAYER" -> return player()
-        "PLAYERS_BY_LANGUAGE"->return  PLAYERS_BY_LANGUAGE()
-    }
-    return null
-}
-
-fun PLAYERS_BY_LANGUAGE(): String {
-    return ""
-}
-
-
-fun player(): String {
-    return ""
-}
-
-fun getFunction() {
-
-}
