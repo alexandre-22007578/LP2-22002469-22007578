19418c07a08632c9f9b29ad0285de0aa4e3ada74
Alexandre Costa <a22007578@alunos.ulht.pt>
2021-12-30 11:50:40 +0000

    começo funções

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -2,24 +2,24 @@ package pt.ulusofona.lp2.deisiGreatGame
 
 enum class CommandType { GET, POST }
 
-fun router(): (CommandType) -> (GameManager, List<String>) -> String? {
+fun router(): (CommandType) -> ((GameManager, List<String>) -> String?)? {
     return ::teste
 }
 
 
-fun teste(type: CommandType): (GameManager, List<String>) -> String? {
-    return if (type==CommandType.GET){
-        ::getters
-    }else {
-        ::postters
+fun teste(type: CommandType): ((GameManager, List<String>) -> String?)? {
+    if (type == CommandType.GET) {
+       return ::getters
+    } else if (type == CommandType.POST) {
+        return ::postters
     }
-
+return null
 }
 
-fun getters(gameManager: GameManager,list: List<String>):String?{
+fun getters(gameManager: GameManager, list: List<String>): String? {
     return ""
 }
 
-fun postters(gameManager: GameManager,list: List<String>):String?{
+fun postters(gameManager: GameManager, list: List<String>): String? {
     return ""
 }
